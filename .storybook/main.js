54ae856f342e5f839db147a670d36576b67bdc4e
deeborges <deyvisonsilvaborges@gmail.com>
2021-02-06 01:44:00 -0300

    feat: stories - tipagem na Main

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-knobs',
-    '@storybook/addon-actions'
+    '@storybook/addon-actions',
+    '@storybook/addon-controls'
   ]
 };
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -3,9 +3,9 @@ import * as styled from './styled';
 const Main = () => (
   <styled.Wrapper>
     <styled.Container>
-      <styled.Logo src={'assets/logo.png'} />
+      <styled.Logo src="/assets/logo.png" alt="logo" />
       <styled.Container>
-        <styled.Eu src={'assets/eu.png'} />
+        <styled.Eu src="/assets/eu.png" alt="eu" />
       </styled.Container>
     </styled.Container>
   </styled.Wrapper>
