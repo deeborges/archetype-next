4813d8ad0e93769023daccb89cad9316117a13cf
Deyvison Borges <deyvisonsilvaborges@gmail.com>
2021-04-25 20:36:11 -0300

    auth

--- src/templates/Auth/styled.ts
+++ src/templates/Auth/styled.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components';
+import media from 'styled-media-query';
 
 export const Wrapper = styled.main`
   display: grid;
@@ -8,23 +9,31 @@ export const Container = styled.main`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   height: 100vh;
+
+  ${media.lessThan('medium')`
+    grid-template-columns: 1fr;
+  `}
 `;
+
 export const Banner = styled.div`
-  align-content: center;
-  justify-content: center;
-  display: grid;
   ${({ theme }) => css`
     background-image: url(/img/banner_001.png);
     background-size: cover;
     background-position: center center;
     color: ${theme.colors.neutrals.white};
     line-height: 1;
+
+    ${media.lessThan('medium')`
+      display: none;
+    `}
   `}
 
   img {
-    align-self: flex-end;
+    margin-top: 6rem;
+    justify-self: center;
   }
 `;
+
 export const Subtitle = styled.h3``;
 
 export const ContentForm = styled.div`
@@ -34,10 +43,17 @@ export const ContentForm = styled.div`
     align-items: center;
     justify-content: center;
     width: 30rem;
+
+    ${media.greaterThan('medium')`
+      display: 38rem;
+    `}
   `}
 `;
 export const Footer = styled.p`
   padding: 1.5rem;
+  ${media.lessThan('medium')`
+      display: none;
+    `}
 `;
 
 export const ContentFormWrapper = styled.div`
